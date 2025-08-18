810faf60a9dda50935b8c7e7c09facfdea6028ef
Frontendland <ferenc@frontendland.com>
2025-08-18 10:28:23 +0200

    ⬆️ Update Webcore to latest version

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// Prevent types from webcoreui/svelte causing build errors
+declare module 'svelte' {
+    export type Snippet = any
+    export class SvelteComponent<T = any> {
+        [key: string]: any
+    }
+}
+
+declare module 'svelte/elements' {
+    export type MouseEventHandler<T = any> = (...args: any[]) => any
+}
